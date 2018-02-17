38b9a267d89f72283e709504e3255f7514d693b4
Phumai Chanphunga <kang081@hotmail.com>
2018-02-17 18:54:12 +0700

    add master

--- postfixToResult.cpp
+++ postfixToResult.cpp
@@ -16,11 +16,9 @@ struct Stack* createStack( unsigned capacity )
 	struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack));
 
 	if (!stack) return NULL;
-
 	stack->top = -1;
 	stack->capacity = capacity;
 	stack->array = (int*) malloc(stack->capacity * sizeof(int));
-
 	if (!stack->array) return NULL;
 
 	return stack;
